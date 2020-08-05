487ce30bfd94acb5607edba8e3c165d5e228b8bf
Dogus Kucukgode <doguskucukgode@gmail.com>
2020-08-05 23:23:51 +0300

    smoke tests added

--- backend/src/main/java/com/doguskucukgode/rss/model/User.java
+++ backend/src/main/java/com/doguskucukgode/rss/model/User.java
@@ -1,10 +1,16 @@
 package com.doguskucukgode.rss.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User implements Serializable {
 
     private String id;
